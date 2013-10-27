14bd629161ce676b4530f7746c24b0f739cb0136
Joshua de Lange <joshua.delange@gmail.com>
2013-10-27 16:10:55 +0100

    let there be rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FussballScoreTracker::Application.config.secret_key_base = '3b01abd09815aac7a4ad7674ce948f21647ef3311e85549b354f44f50829e065707d5ecade6d5d00ecbb238f37a4d6a7cb5ab2b59a41797cba2c0fcee4c70abf'
