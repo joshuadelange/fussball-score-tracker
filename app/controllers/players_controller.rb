1f95c4c93c9221d0a5f950b3d2e378b890fd5538
Joshua de Lange <joshua.delange@gmail.com>
2013-10-28 02:06:34 +0100

    adds autocomplete!

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,15 @@
 class PlayersController < ApplicationController
 
   def index
-    @players = Player.all()
-    @players.sort! { |p1, p2| p2.score <=> p1.score }
+
+    unless request[:q].blank?
+      @players = Player.where("LOWER(name) like LOWER(?)", "%#{request[:q]}%")
+      render :json => @players
+    else
+      @players = Player.all()
+      @players.sort! { |p1, p2| p2.score <=> p1.score }
+    end
+
   end
 
   def show
